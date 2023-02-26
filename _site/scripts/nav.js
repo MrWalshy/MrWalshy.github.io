aaad21df09f26e0f8cff150571c920a3d31bdb6e
MrWalshyType2 <mwalsh@qa.com>
2023-02-26 01:01:27 +0000

    Deleted unecessary build artifacts and old site data, added ignore for _site folder

--- _site/scripts/nav.js
+++
@@ -1,7 +0,0 @@
-const navbar = document.querySelector(".navbar");
-const toggle = navbar.querySelector("#toggle");
-const navContent = navbar.querySelector(".nav-content");
-
-toggle.addEventListener("click", event => {
-    navContent.classList.toggle("show");
-});
--- frontend/templates/code.md
+++
@@ -1,9 +0,0 @@
-@template(name="fn_header" args="value")
-```
-<h3><code>@value;</code></h3>
-```
-
-@template(name="code" args="value")
-```
-<pre><code>@value;</code></pre>
-```
